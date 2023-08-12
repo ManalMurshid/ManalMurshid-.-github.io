73d1c136cfe1c34514b3339fb006e76e96340ac5
ManalMurshid <136351098+ManalMurshid@users.noreply.github.com>
2023-08-12 21:48:40 +0530

    Add files via upload

--- with_animation_db.sql
+++ with_animation_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2023 at 09:55 AM
+-- Generation Time: Aug 12, 2023 at 06:15 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,24 +24,112 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `form`
+-- Table structure for table `contact_form`
 --
 
-CREATE TABLE `form` (
+CREATE TABLE `contact_form` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `phoneNumber` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `form`
+-- Dumping data for table `contact_form`
 --
 
-INSERT INTO `form` (`name`, `email`, `phoneNumber`) VALUES
-('Jaladhini', 'jala5@gmail.com', 762453789),
-('Bashini', 'mirando12@gmail.com', 715678976),
-('Raj', 'rajs9@gmail.com', 764352567),
-('Zara', 'zarazain7@gmail.com', 782543543);
+INSERT INTO `contact_form` (`id`, `name`, `email`, `phoneNumber`) VALUES
+(1, 'Jaladhini', 'jala5@gmail.com', 762453789),
+(2, 'Bashini', 'mirando12@gmail.com', 715678976),
+(3, 'Raj', 'rajs9@gmail.com', 764352567),
+(4, 'Zara', 'zarazain7@gmail.com', 782543543),
+(7, 'admin', 'admin@gmail.com', 123567894);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `creation`
+--
+
+CREATE TABLE `creation` (
+  `id` int(255) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `creation`
+--
+
+INSERT INTO `creation` (`id`, `title`, `description`, `url`) VALUES
+(100, 'Portfolio Website', 'A small portfolio website for university project', 'https:www.google.com'),
+(101, 'Hostel Management System', 'A small hostel M=management S=system for university library', 'https:www.google.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`, `uid`) VALUES
+('admin@gmail.com', '123456@', 1),
+('lanam@gmail.com', 'abcd@123', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `creation`
+--
+ALTER TABLE `creation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `creation`
+--
+ALTER TABLE `creation`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
